db9adb26e45472b8d1aadac1e6b20bc350a2cf20
Shindu Astawa <shinduastawa@gmail.com>
2025-05-09 19:21:26 +0800

    update env servicekey

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,11 +1,25 @@
-import admin from "firebase-admin";
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
-const serviceAccount = require("./config/serviceAccountKey.json");
+// import admin from "firebase-admin";
+// import { createRequire } from "module";
+// const require = createRequire(import.meta.url);
+// const serviceAccount = require("./config/serviceAccountKey.json");
 
-// Inisialisasi Firebase Admin
-const firebaseAdmin = admin.initializeApp({
+// // Inisialisasi Firebase Admin
+// const firebaseAdmin = admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
+
+// export default firebaseAdmin;
+
+import admin from "firebase-admin";
+import dotenv from "dotenv";
+dotenv.config();  // Pastikan file .env dimuat
+
+// Parsing konfigurasi Firebase dari variabel lingkungan
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
+
+// Inisialisasi Firebase Admin dengan kredensial dari serviceAccount
+admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
-export default firebaseAdmin;
+export default admin;
