a846720171e191c0c62776e4d9291f8907af33b8
Shindu Astawa <shinduastawa@gmail.com>
2025-05-09 20:24:26 +0800

    firebase config uodate

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,19 +1,9 @@
-// import admin from "firebase-admin";
-// import { createRequire } from "module";
-// const require = createRequire(import.meta.url);
-// const serviceAccount = require("./config/serviceAccountKey.json");
-
-// // Inisialisasi Firebase Admin
-// const firebaseAdmin = admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-// });
-
-// export default firebaseAdmin;
-
 import admin from "firebase-admin";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const serviceAccount = require("./config/serviceAccountKey.json");
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
-
+// Inisialisasi Firebase Admin
 const firebaseAdmin = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
