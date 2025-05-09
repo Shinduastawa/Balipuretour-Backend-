7a94cd85d5f4665810aba6b9e1c073ad2e890f40
Shindu Astawa <shinduastawa@gmail.com>
2025-05-09 20:41:44 +0800

    chage service key call

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,11 +1,10 @@
 import admin from "firebase-admin";
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
-const serviceAccount = require("./config/serviceAccountKey.json");
 
-// Inisialisasi Firebase Admin
+const rawConfig = JSON.parse(process.env.FIREBASE_CONFIG);
+rawConfig.private_key = rawConfig.private_key.replace(/\\n/g, "\n");
+
 const firebaseAdmin = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(rawConfig),
 });
 
 export default firebaseAdmin;
