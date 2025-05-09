46ea6a7762b4bffbe291d73a3230b33e232b9d13
Shindu Astawa <shinduastawa@gmail.com>
2025-05-09 22:32:37 +0800

    chage db config

--- config/Database.js
+++ config/Database.js
@@ -7,18 +7,47 @@
 
 // export default db;
 
-import { Sequelize } from "sequelize";
+// import { Sequelize } from "sequelize";
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// const db = new Sequelize(process.env.DATABASE_URL, {
+//   dialect: "mysql",
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false,
+//     }
+//   }
+// });
+
+// export default db;
+
 import dotenv from "dotenv";
 dotenv.config();
 
-const db = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "mysql",
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    }
-  }
+import { Sequelize } from "sequelize";
+
+console.log("📦 DB CONFIG:", {
+  DB_NAME: process.env.DB_NAME,
+  DB_USER: process.env.DB_USER,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: process.env.DB_PORT
 });
 
+const db = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    dialect: "mysql",
+    logging: false
+  }
+);
+
 export default db;
+
+
