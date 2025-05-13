d847c8e702acec25e7cfda1cce170308c934972d
Shindu Astawa <shinduastawa@gmail.com>
2025-05-13 08:46:32 +0800

    Mempebarui Model Table Name iboxes

--- models/InboxModel.js
+++ models/InboxModel.js
@@ -2,19 +2,26 @@
 import { DataTypes } from "sequelize";
 import db from "../config/Database.js";
 
-const Inbox = db.define("Inbox", {
-  type: {
-    type: DataTypes.ENUM("user_login", "booking_full", "new_booking", "payment_success"),
-    allowNull: false,
+const Inbox = db.define(
+  "Inbox",
+  {
+    type: {
+      type: DataTypes.ENUM("user_login", "booking_full", "new_booking", "payment_success"),
+      allowNull: false,
+    },
+    message: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    read: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
   },
-  message: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  read: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: false,
+  {
+    freezeTableName: true,
+    tableName: "inboxes",
   }
-});
+);
 
 export default Inbox;
