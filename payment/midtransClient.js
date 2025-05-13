1c135a1944b344d43db4b5992bdb6a343f9cbfde
Shindu Astawa <shinduastawa@gmail.com>
2025-05-13 14:25:11 +0800

    flase to true

--- payment/midtransClient.js
+++ payment/midtransClient.js
@@ -2,7 +2,7 @@
 import midtransClient from 'midtrans-client';
 
 let snap = new midtransClient.Snap({
-    isProduction: false, // Ganti ke true jika sudah live
+    isProduction: true, // Ganti ke true jika sudah live
     serverKey: 'SB-Mid-server-pR556x1VaXnmJQZ9YTE_cxJl',
     clientKey: 'SB-Mid-client-Aniqdi4-3mCKWtB9'
 });
