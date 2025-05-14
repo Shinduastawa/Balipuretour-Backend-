71324b4034efdfe1269a56de6cb654d90d334b21
Shindu Astawa <shinduastawa@gmail.com>
2025-05-15 00:48:37 +0800

    add new expriy time

--- controllers/Payment.js
+++ controllers/Payment.js
@@ -5,7 +5,6 @@ import utc from "dayjs/plugin/utc";
 import timezone from "dayjs/plugin/timezone";
 import Transaction from "../models/TransactionModel.js";
 import { sendInvoiceEmail, generateInvoicePDF } from "../services/sendInvoiceEmail.js";
-
 dotenv.config();
 
 dayjs.extend(utc);
