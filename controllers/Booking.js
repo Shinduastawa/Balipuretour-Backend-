a36f97531bd174b05f6d71cd76c7af9e31691bda
Shindu Astawa <shinduastawa@gmail.com>
2025-05-14 13:01:19 +0800

    add email booking to admin

--- controllers/Booking.js
+++ controllers/Booking.js
@@ -1,13 +1,8 @@
 import Booking from "../models/BookingModel.js";
 import PackageTour from "../models/PackgeTourModel.js";
-import Inbox from "../models/InboxModel.js"; // ⬅️ Tambahin ini di atas
-import AvailableDates from "../models/AvailableDatesModel.js";
-import Booking from "../models/BookingModel.js";
-import PackageTour from "../models/PackgeTourModel.js";
 import Inbox from "../models/InboxModel.js";
 import AvailableDates from "../models/AvailableDatesModel.js";
-import nodemailer from "nodemailer"; // ✅ Tambahkan ini
-
+import nodemailer from "nodemailer";
 // ✅ Buat Booking Baru
 export const createBooking = async (req, res) => {
   try {
